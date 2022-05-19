7e65e58f0a78b1ece3f35176c40b0cf17dc5faf4
David Neuman <david.m.neuman@gmail.com>
2022-05-19 16:47:23 -0400

    Add functionality for supporting remaining basic CRUD operations for tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((t) => t.id === id);
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string) {
+    console.log(id);
+    console.log(this.tasks);
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
